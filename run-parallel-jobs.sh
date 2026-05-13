cd2386f58655124d95746bf46f1331f3997f7a63
Sung-Shik Jongmans <sung-shik.jongmans@swat.engineering>
2026-05-13 10:20:23 +0200

    Update `run-parallel-jobs.sh` to use the same projects as in the GHA workflow

--- run-parallel-jobs.sh
+++ run-parallel-jobs.sh
@@ -13,14 +13,22 @@ function runChecker() {
     java -Drascal.monitor.batch -jar $RASCAL_JAR Main --update --repoFolder "$BIG_DIR" $CLEAN $EXTRA_ARGS --tests $@ >"$name.log" 2>&1  &
 }
 
-## first we have to run rascal
-echo "Running rascal first, as everything depends on it"
+echo "To be started in parallel jobs:"
+echo " 1. *-all: \`rascal-all\`, followed by \`rascal-lsp-all\` (note: fails on Windows, because \`java -jar ...\` command too long)"
+echo " 2. rascal: \`rascal\` (as all libraries depend on it), followed by the libraries in two parallel jobs"
+
+# (1)
+runChecker '*-all' 'rascal-all' 'rascal-lsp-all' # Fails on Windows
+
+# (2)
 runChecker 'rascal' 'rascal'
-wait
+wait $!
 echo "Tail out output from rascal.log"
+echo "\`\`\`"
 tail 'rascal.log'
+echo "\`\`\`"
 
 echo "Rascal is done, now lets run the rest in 2 parallel jobs"
-runChecker 'libraries' 'flybytes' 'php-analysis' 'rascal-git' 'salix-core' 'drambiguity' 'salix-contrib' 'rascal-all'
-runChecker 'core-and-lsp' 'typepal' 'rascal-core' 'rascal-lsp'
+runChecker 'libraries' 'flybytes' 'salix-core' 'salix-contrib' 'drambiguity' 'rascal-git' 'php-analysis' 'typepal' 'clair' 'java-air' 'python-air' 'rascal-lucene'
+runChecker 'typepal-and-lsp' 'typepal' 'rascal-lsp' # `rascal-lsp` requires TPLs to be available in the jar located by $RASCAL_JAR
 wait
